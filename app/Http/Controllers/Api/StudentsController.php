0a95aa37c492eda82816d9a75d6b099bd4767a5c
muhammadanees011 <itsanees011@gmail.com>
2024-01-24 18:39:36 +0500

    Debugging

--- app/Http/Controllers/Api/StudentsController.php
+++ app/Http/Controllers/Api/StudentsController.php
@@ -98,9 +98,9 @@ class StudentsController extends Controller
         // ->select('SHOW TABLES');
         $tables = DB::connection('remote_mysql')->table('ebStudent')->get();
         $users = User::get();
+        return $users;
         $tableEmails = $tables->pluck('eMail')->toArray();
         $userEmails = $users->pluck('email')->toArray();
-        return $userEmails;
         // Identify emails that are in $tables but not in $users
         $newEmails = array_diff($tableEmails, $userEmails);
         // Fetch the records corresponding to the new emails
