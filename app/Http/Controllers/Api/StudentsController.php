f0ffac4f72b9ad5d24f1eca7f525361c9f586347
muhammadanees011 <itsanees011@gmail.com>
2024-01-24 18:45:31 +0500

    Debugging

--- app/Http/Controllers/Api/StudentsController.php
+++ app/Http/Controllers/Api/StudentsController.php
@@ -98,7 +98,8 @@ class StudentsController extends Controller
         // ->select('SHOW TABLES');
         $tables = DB::connection('remote_mysql')->table('ebStudent')->get();
         $users = User::get();
-        return $users;
+        $students = Student::count();
+        return $students;
         $tableEmails = $tables->pluck('eMail')->toArray();
         $userEmails = $users->pluck('email')->toArray();
         // Identify emails that are in $tables but not in $users
