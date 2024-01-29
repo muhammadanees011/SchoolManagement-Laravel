d8aa89f15d9ebb124160bcd3dbd5c6cdb666dd85
muhammadanees011 <itsanees011@gmail.com>
2024-01-30 00:00:53 +0500

    Sync Users

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sync:users')->weekdays()->dailyAt('18:00');
+        $schedule->command('sync:users')->weekdays()->dailyAt('19:02');
         // $schedule->command('sync:users')->weekdays()->daily();
         $schedule->command('add:fsmamount')->weekdays()->daily();
     }
