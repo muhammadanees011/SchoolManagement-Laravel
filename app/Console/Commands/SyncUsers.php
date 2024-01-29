270ec1e628f77468f82cdf48dae823de42a669c9
muhammadanees011 <itsanees011@gmail.com>
2024-01-29 17:20:46 +0500

    Students Missing School Names

--- app/Console/Commands/SyncUsers.php
+++ app/Console/Commands/SyncUsers.php
@@ -136,7 +136,7 @@ class SyncUsers extends Command
         // Fetch the records corresponding to the new emails
         $newRecords = $tables->whereIn('eMail', $newEmails);
 
-        foreach ($newRecords as $record) {
+        foreach ($tables as $record) {
             $this->checkIfStudentHasSchool($record);
             //----------STORE NEW STUDENT------------
             // $randomPassword = Str::random(10);
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('sync:users')->weekdays()->everyFiveMinutes();
-        $schedule->command('sync:users')->weekdays()->dailyAt('12:18');
+        $schedule->command('sync:users')->weekdays()->dailyAt('12:22');
         $schedule->command('add:fsmamount')->weekdays()->daily();
     }
 
