b86381bd297ea7ed2556a127ee0201398a966ece
muhammadanees011 <itsanees011@gmail.com>
2024-01-29 17:17:09 +0500

    Students Missing School Names

--- app/Console/Commands/SyncUsers.php
+++ app/Console/Commands/SyncUsers.php
@@ -13,6 +13,7 @@ use App\Models\Wallet;
 use App\Models\User;
 use App\Models\Student;
 use App\Models\School;
+use App\Models\Organization;
 use App\Models\Staff;
 
 class SyncUsers extends Command
@@ -74,6 +75,13 @@ class SyncUsers extends Command
                 if($school){
                     $school->students_count=$school->students_count + 1;
                     $school->save();
+                }else{
+                    $organization=Organization::where('name','Education Training Collective')->first();
+                    $newSchool=new School();
+                    $newSchool->organization_id=$organization->id;
+                    $newSchool->title=$record->site;
+                    $newSchool->save();
+                    $school=$newSchool;
                 }
                 $student=new Student();
                 $student->user_id = $userId;
@@ -129,60 +137,68 @@ class SyncUsers extends Command
         $newRecords = $tables->whereIn('eMail', $newEmails);
 
         foreach ($newRecords as $record) {
+            $this->checkIfStudentHasSchool($record);
             //----------STORE NEW STUDENT------------
-            $randomPassword = Str::random(10);
-            $studentName = $record->firstName . ' ' . $record->surname;
-            try{
-                $userId=DB::table('users')->insertGetId([
-                    'first_name' => $record->firstName,
-                    'last_name' => $record->surname,
-                    'email' => $record->eMail,
-                    'password' => bcrypt($randomPassword),
-                    'role' => 'staff',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-                //-----------SAVE STAFF----------------
-                // $school=School::where('title',$record->site)->first();
-                $school = School::where('title', 'like', '%' . $record->site . '%')->first();
-                if($school){
-                    $school->teachers_count=$school->teachers_count + 1;
-                    $school->save();
-                }
-                $staff=new Staff();
-                $staff->user_id = $userId;
-                if($school){
-                    $staff->school_id = $school->id;
-                }
-                $student->staff_id = $record->loginID;
-                $staff->upn = $record->UPN;
-                $staff->mifare_id = $record->miFareID;
-                $staff->site = $record->site;
-                $staff->save();
-                //----------SEND WELCOME MAIL--------------
-                $mailData = [
-                    'title' => 'Congratulations you have successfully created your StudentPay account!',
-                    'body' => $randomPassword,
-                    'user_name'=> $studentName,
-                ];
-                // Mail::to($record->eMail)->send(new WelcomeEmail($mailData));
-                //----------CREATE STRIPE CUSTOMER------------
-                $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
-                $customer=$stripe->customers->create([
-                'name' => $studentName,
-                'email' => $record->eMail,
-                ]);
-                $user=User::where('id',$userId)->first();
-                $user->stripe_id=$customer->id;
-                $user->created_at=now();
-                $user->updated_at=now();
-                $user->save();
-                //----------CREATE STUDENT WALLET-------------
-                $userWallet=new Wallet();
-                $userWallet->user_id=$userId;
-                $userWallet->save();
-                } catch (\Exception $e) {
-            }
+            // $randomPassword = Str::random(10);
+            // $studentName = $record->firstName . ' ' . $record->surname;
+            // try{
+            //     $userId=DB::table('users')->insertGetId([
+            //         'first_name' => $record->firstName,
+            //         'last_name' => $record->surname,
+            //         'email' => $record->eMail,
+            //         'password' => bcrypt($randomPassword),
+            //         'role' => 'staff',
+            //         'created_at' => now(),
+            //         'updated_at' => now(),
+            //     ]);
+            //     //-----------SAVE STAFF----------------
+            //     // $school=School::where('title',$record->site)->first();
+            //     $school = School::where('title', 'like', '%' . $record->site . '%')->first();
+            //     if($school){
+            //         $school->teachers_count=$school->teachers_count + 1;
+            //         $school->save();
+            //     }else{
+            //         $organization=Organization::where('name','Education Training Collective')->first();
+            //         $newSchool=new School();
+            //         $newSchool->organization_id=$organization->id;
+            //         $newSchool->title=$record->site;
+            //         $newSchool->save();
+            //         $school=$newSchool;
+            //     }
+            //     $staff=new Staff();
+            //     $staff->user_id = $userId;
+            //     if($school){
+            //         $staff->school_id = $school->id;
+            //     }
+            //     $student->staff_id = $record->loginID;
+            //     $staff->upn = $record->UPN;
+            //     $staff->mifare_id = $record->miFareID;
+            //     $staff->site = $record->site;
+            //     $staff->save();
+            //     //----------SEND WELCOME MAIL--------------
+            //     $mailData = [
+            //         'title' => 'Congratulations you have successfully created your StudentPay account!',
+            //         'body' => $randomPassword,
+            //         'user_name'=> $studentName,
+            //     ];
+            //     // Mail::to($record->eMail)->send(new WelcomeEmail($mailData));
+            //     //----------CREATE STRIPE CUSTOMER------------
+            //     $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+            //     $customer=$stripe->customers->create([
+            //     'name' => $studentName,
+            //     'email' => $record->eMail,
+            //     ]);
+            //     $user=User::where('id',$userId)->first();
+            //     $user->stripe_id=$customer->id;
+            //     $user->created_at=now();
+            //     $user->updated_at=now();
+            //     $user->save();
+            //     //----------CREATE STUDENT WALLET-------------
+            //     $userWallet=new Wallet();
+            //     $userWallet->user_id=$userId;
+            //     $userWallet->save();
+            //     } catch (\Exception $e) {
+            // }
     
         }
     }
@@ -196,6 +212,27 @@ class SyncUsers extends Command
                 return false;
             }else{
                 $user->delete();
+                return true;
+            }
+        }else{
+            return true;
+        }
+        
+    }
+
+    public function checkIfStudentHasSchool($record){
+        $user=User::where('email',$record->eMail)->first();
+        if($user){
+            $student=Student::where('user_id',$user->id)->first();
+            if($student->school_id){
+                // do nothing
+                return false;
+            }else{
+                $organization=Organization::where('name','Education Training Collective')->first();
+                $newSchool=new School();
+                $newSchool->organization_id=$organization->id;
+                $newSchool->title=$record->site;
+                $newSchool->save();
                 return true;
             }
         }else{
