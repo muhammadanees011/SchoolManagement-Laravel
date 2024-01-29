8b64cfbfddb07849401708448609d8fe89ed3315
muhammadanees011 <itsanees011@gmail.com>
2024-01-29 18:34:37 +0500

    Students Missing School Names

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('sync:users')->weekdays()->everyFiveMinutes();
-        $schedule->command('sync:users')->weekdays()->dailyAt('13:32');
+        $schedule->command('sync:users')->weekdays()->dailyAt('13:36');
         $schedule->command('add:fsmamount')->weekdays()->daily();
     }
 
