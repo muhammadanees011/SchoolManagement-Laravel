ceda536b67d979496403acc699cb73ade7e6feba
muhammadanees011 <itsanees011@gmail.com>
2024-02-15 21:11:00 +0500

    Cron Job

--- app/Http/Controllers/Api/StudentsController.php
+++ app/Http/Controllers/Api/StudentsController.php
@@ -187,7 +187,7 @@ class StudentsController extends Controller
         $newUsers=User::whereIn('id',$newIds)->pluck('email')->toArray();
         $otherUsers=DB::connection('remote_mysql')->table('ebStudent')->whereIn('eMail',$newUsers)->get();
         // return $otherUsers;
-        return $tables;
+        return $totalUsers;
 
         // Identify emails that are in $tables but not in $users
         $newEmails = array_diff($tableEmails, $userEmails);
