88cc74e40b8aecc0a5b7c90ad6b38b7a0b4efb7e
muhammadanees011 <itsanees011@gmail.com>
2024-02-15 18:08:09 +0500

    Cron Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sync:users')->weekdays()->dailyAt('19:58');
+        $schedule->command('sync:users')->weekdays()->dailyAt('13:10');
         // $schedule->command('sync:users')->weekdays()->daily();
         $schedule->command('add:fsmamount')->weekdays()->daily();
     }
