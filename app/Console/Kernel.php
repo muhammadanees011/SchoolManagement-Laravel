4fe2b23e735fe1dce33c2890f50d531f81257f62
muhammadanees011 <117478335+muhammadanees011@users.noreply.github.com>
2024-03-08 17:30:28 +0500

    Cron time changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sync:users')->weekdays()->dailyAt('07:00');
+        $schedule->command('sync:users')->weekdays()->dailyAt('06:00');
         // $schedule->command('add:fsmamount')->weekdays()->daily();
     }
 
