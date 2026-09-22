2e257083bc162121852dd86a4df968eab8eec872
muhammadanees011 <117478335+muhammadanees011@users.noreply.github.com>
2026-09-22 17:47:49 +0500

    scheduler time changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sync:users')->weekdays()->dailyAt('06:00');
+        $schedule->command('sync:users')->weekdays()->everyTenMinutes();
+        // $schedule->command('sync:users')->weekdays()->dailyAt('06:00');
         // $schedule->command('add:fsmamount')->weekdays()->daily();
     }
 
